2b58cc5f8cc68327ee776336f4d3ecad9aa99586
Piyush Choudhary <mercmerc961@gmail.com>
2024-10-03 05:10:02 +0530

    Add files via upload

---
+++ Src/EXTI_IRQHandlers.c
@@ -0,0 +1,63 @@
+/**
+ * 			CAREFUL MODIFCATION ALLOWED
+ *
+ * @brief This file contains the EXTI IRQ handlers for specific processor, the code written here can be updates as per specific MCU and as per the needs.
+ */
+
+#include "EXTI_IRQHandlers.h"
+
+
+
+/**
+ * @brief IRQHandler for EXTI line 1
+ * @param void
+ * @retvalue void
+ */
+void EXTI1_IRQHandler(void){
+
+}
+
+/**
+ * @brief IRQHandler for EXTI line 2
+ * @param void
+ * @retvalue void
+ */
+void EXTI2_IRQHandler(void){
+
+}
+
+/**
+ * @brief IRQHandler for EXTI line 3
+ * @param void
+ * @retvalue void
+ */
+void EXTI3_IRQHandler(void){
+
+}
+
+/**
+ * @brief IRQHandler for EXTI line 4
+ * @param void
+ * @retvalue void
+ */
+void EXTI4_IRQHandler(void){
+
+}
+
+/**
+ * @brief IRQHandler for EXTI line 5 to 9
+ * @param void
+ * @retvalue void
+ */
+void EXTI9_5_IRQHandler(void){
+
+}
+
+/**
+ * @brief IRQHandler for EXTI line 10 to 15
+ * @param void
+ * @retvalue void
+ */
+void EXTI15_10_IRQHandler(void){
+
+}
